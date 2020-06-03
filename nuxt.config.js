edd8bc82e1a70c89d2842685de495e2ca0dfeb94
yu.feshenko <yu.feshenko@ekf.su>
2020-06-03 17:19:59 +0300

    Initial setup

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'bop24 — Доставка продуктов по Москве и Московской области',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'bop24 — Доставка продуктов по Москве и Московской области'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#18d4c9' },
+  /*
+   ** Global CSS
+   */
+  css: [{ src: '~assets/sass/app.scss', lang: 'scss' }],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['bootstrap-vue/nuxt', '@nuxtjs/svg-sprite'],
+  /*
+   ** Build configuration
+   */
+
+  bootstrapVue: {
+    bootstrapCSS: false,
+    bootstrapVueCSS: false,
+    componentPlugins: [
+      'LayoutPlugin',
+      'LinkPlugin',
+      'ButtonPlugin',
+      'FormPlugin',
+      'FormGroupPlugin',
+      'FormInputPlugin',
+      'FormCheckboxPlugin',
+      'FormTextareaPlugin',
+      'SpinnerPlugin',
+      'ToastPlugin'
+    ]
+  },
+
+  svgSprite: {
+    input: '~/assets/images/icons'
+  },
+
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}

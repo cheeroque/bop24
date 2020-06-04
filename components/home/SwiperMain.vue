a972a59d363f6b946d0af14e438ff77f24054335
yu.feshenko <yu.feshenko@ekf.su>
2020-06-04 17:56:28 +0300

    Added Main Swiper

---
+++ components/home/SwiperMain.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="swiper swiper-main">
+    <swiper ref="swiperMain" :options="swiperOptions">
+      <swiper-slide v-for="i in 10" :key="i">
+        <div class="text-center">
+          {{ i }}
+        </div>
+      </swiper-slide>
+      <div slot="pagination" class="swiper-pagination"></div>
+      <div slot="button-prev" class="swiper-button-prev"></div>
+      <div slot="button-next" class="swiper-button-next"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      swiperOptions: {
+        loop: true,
+        grabCursor: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets'
+        }
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <main>
     <header-tags></header-tags>
+    <swiper-main></swiper-main>
   </main>
 </template>
 
 <script>
 import HeaderTags from '@/components/header/HeaderTags'
+import SwiperMain from '@/components/home/SwiperMain'
+
 export default {
   components: {
-    HeaderTags
+    HeaderTags,
+    SwiperMain
   }
 }
 </script>

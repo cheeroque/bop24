89a66c53054e479d6ef679cb9f15e9741701ebb5
Cheeroque <ronerycamel@gmail.com>
2020-06-13 15:51:36 +0300

    Added order item page

---
+++ components/cards/CardOrderItem.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="card card-order-item">
+    <b-row class="mx-n2">
+      <b-col cols="3" lg="2" class="px-2 pr-lg-4">
+        <div class="card-img-wrapper embed-responsive embed-responsive-1by1">
+          <div class="embed-responsive-item d-flex">
+            <img :src="`/images/products/${item.img}.jpg`" class="card-img" />
+          </div>
+        </div>
+        <div class="text-center mt-1 d-lg-none">
+          <div class="card-rating d-inline-flex">
+            <svg-icon
+              v-for="i in item.rating"
+              :key="`star-${i}-full`"
+              name="rating"
+              width="11"
+              height="11"
+              class="icon-rating icon-rating-full"
+            />
+            <svg-icon
+              v-for="i in 5 - item.rating"
+              :key="`star-${i}`"
+              name="rating"
+              width="11"
+              height="11"
+              class="icon-rating"
+            />
+          </div>
+        </div>
+      </b-col>
+      <b-col cols="9" lg="10" class="px-2 pl-lg-0">
+        <div class="card-body">
+          <b-row align-v="center" class="h-100">
+            <b-col cols="12" lg="5" class="mb-2 mb-lg-auto">
+              <div class="card-rating d-none d-lg-flex">
+                <svg-icon
+                  v-for="i in item.rating"
+                  :key="`star-${i}-full`"
+                  name="rating"
+                  width="11"
+                  height="11"
+                  class="icon-rating icon-rating-full"
+                />
+                <svg-icon
+                  v-for="i in 5 - item.rating"
+                  :key="`star-${i}`"
+                  name="rating"
+                  width="11"
+                  height="11"
+                  class="icon-rating"
+                />
+              </div>
+              <p class="font-size-lg-2 font-weight-medium mb-0">
+                {{ item.title }}
+              </p>
+            </b-col>
+            <b-col cols="12" lg="4">
+              <div
+                class="d-flex flex-column flex-lg-column-reverse text-gray-700"
+              >
+                <p class="mb-2 mb-lg-0">
+                  Количество:
+                  <span class="font-size-2 font-weight-medium text-secondary">
+                    {{ item.count }}
+                  </span>
+                </p>
+                <p class="mb-2">
+                  Цена:
+                  <span class="font-size-2 font-weight-medium text-secondary">
+                    {{ item.price }}&nbsp;р.
+                  </span>
+                </p>
+              </div>
+            </b-col>
+            <b-col cols="12" lg="3">
+              <b-row align-v="center" no-gutters>
+                <b-col
+                  cols="6"
+                  lg="12"
+                  order-lg="2"
+                  class="text-gray-700 text-lg-center"
+                >
+                  <b-link
+                    to="/catalog/item"
+                    class="text-reset text-decoration-none"
+                  >
+                    Оставить отзыв
+                  </b-link>
+                </b-col>
+                <b-col cols="6" lg="12" order-lg="1" class="mb-lg-2">
+                  <transition name="fade" mode="out-in">
+                    <div v-if="!inCart" key="button" class="flex-fill">
+                      <b-button
+                        variant="primary"
+                        class="btn-cart"
+                        @click="addToCart"
+                        block
+                      >
+                        <svg-icon
+                          name="cart"
+                          width="22"
+                          height="20"
+                          class="mr-3 my-n1 d-none d-xl-inline-block"
+                        />
+                        <span class="caption">
+                          В&nbsp;корзину
+                        </span>
+                      </b-button>
+                    </div>
+                    <div v-else key="spinbutton" class="btn-cart">
+                      <b-form-spinbutton
+                        v-model="count"
+                        min="0"
+                        @change="setCount"
+                      >
+                      </b-form-spinbutton>
+                    </div>
+                  </transition>
+                </b-col>
+              </b-row>
+            </b-col>
+          </b-row>
+        </div>
+      </b-col>
+    </b-row>
+    <div class="card-img-wrapper"></div>
+    <div v-if="item.discount" class="card-badge">–{{ item.discount }}%</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      inCart: false,
+      count: 1
+    }
+  },
+  methods: {
+    addToCart() {
+      if (this.count < 1) this.count = 1
+      this.inCart = true
+    },
+    setCount() {
+      if (this.count < 1) this.inCart = false
+    }
+  }
+}
+</script>
---
+++ pages/account/orders/item.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <div class="container">
+      <b-breadcrumb
+        :items="breadcrumbs"
+        class="d-none d-md-flex"
+      ></b-breadcrumb>
+
+      <b-row align-v="center" class="mb-4 d-none d-md-flex">
+        <b-col lg="4" xl="3">
+          <h2 class="font-size-4 mb-0">
+            #личный кабинет
+          </h2>
+        </b-col>
+        <b-col lg="8" xl="9">
+          <b-link to="/account/orders" class="text-dark text-decoration-none">
+            <svg-icon name="arrow-backlink" width="19" height="7" />
+            <span class="ml-2">
+              Все заказы
+            </span>
+          </b-link>
+        </b-col>
+      </b-row>
+    </div>
+    <div class="bg-lg-gray-50">
+      <div class="container">
+        <b-row>
+          <b-col md="6" lg="4" xl="3" class="d-none d-md-block">
+            <account-menu></account-menu>
+          </b-col>
+          <b-col md="6" lg="8" xl="9">
+            <b-row align-v="center" class="mb-3 mt-lg-4">
+              <b-col cols="12" lg="4">
+                <b-row align-v="center">
+                  <b-col cols="7" lg="12">
+                    <b-button
+                      to="/account/orders"
+                      variant="link"
+                      class="account-backlink mb-2 d-md-none"
+                    >
+                      <svg-icon name="arrow-backlink" width="19" height="7" />
+                      <span class="caption">
+                        Заказ
+                        <span class="font-weight-medium text-primary">
+                          №&nbsp;754759
+                        </span>
+                      </span>
+                    </b-button>
+                    <p class="mb-2 d-none d-md-block">
+                      Заказ
+                      <b-link
+                        to="/account/orders/item"
+                        class="font-weight-medium"
+                      >
+                        №&nbsp;754759
+                      </b-link>
+                    </p>
+                  </b-col>
+                  <b-col cols="5" lg="12" class="text-right text-lg-left">
+                    <p class="font-weight-light text-gray-500 mb-2">
+                      от&nbsp;03.12.2020
+                    </p>
+                  </b-col>
+                </b-row>
+              </b-col>
+              <b-col cols="12" lg="4" xl="5">
+                <b-row align-v="center">
+                  <b-col cols="5" lg="12">
+                    <p class="mb-lg-2">
+                      Сумма:
+                      <span class="font-weight-medium">6&nbsp;897&nbsp;р.</span>
+                    </p>
+                  </b-col>
+                  <b-col cols="7" lg="12" class="text-right text-lg-left">
+                    <p class="mb-lg-2">
+                      Баллов за&nbsp;покупку:
+                      <span class="font-weight-medium text-primary">897</span>
+                    </p>
+                  </b-col>
+                </b-row>
+              </b-col>
+              <b-col cols="6" md="7" lg="4" xl="3">
+                <div class="mb-2">
+                  <b-button
+                    variant="outline-primary"
+                    class="d-flex flex-center px-1 line-height-120"
+                    block
+                  >
+                    <svg-icon
+                      name="refresh"
+                      width="20"
+                      height="20"
+                      class="mr-2 my-n1"
+                    />
+                    Повторить заказ
+                  </b-button>
+                </div>
+              </b-col>
+            </b-row>
+
+            <div class="mb-3 mb-lg-5">
+              <card-order-item
+                v-for="i in 6"
+                :key="`order-item-${i}`"
+                :item="orderItem"
+                class="mb-3 mb-lg-2"
+              ></card-order-item>
+            </div>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AccountMenu from '@/components/account/AccountMenu'
+import CardOrderItem from '@/components/cards/CardOrderItem'
+
+export default {
+  layout: 'account',
+  components: {
+    AccountMenu,
+    CardOrderItem
+  },
+  data() {
+    return {
+      breadcrumbs: [
+        { text: 'Главная', href: '/' },
+        { text: 'Личный кабинет', to: '/account' },
+        { text: 'Заказы', to: '/account/orders' },
+        { text: 'Заказ №\xA0754759', to: '/account/orders/item', active: true }
+      ],
+      orderItem: {
+        title: 'Макароны без яиц Antico Pastificio “Пенне Дзита Ригате” 500 г',
+        img: 'pasta',
+        price: '162,50',
+        count: 2,
+        rating: 5
+      }
+    }
+  }
+}
+</script>

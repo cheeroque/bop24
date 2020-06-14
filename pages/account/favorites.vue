717df44734948cc8bd0916a37637e2f4e23a7fee
Cheeroque <ronerycamel@gmail.com>
2020-06-14 13:27:16 +0300

    Added Account favorites page

--- pages/account/favorites.vue
+++ pages/account/favorites.vue
@@ -6,9 +6,37 @@
         class="d-none d-md-flex"
       ></b-breadcrumb>
 
-      <h2 class="font-size-4 mb-4 d-none d-md-flex">
-        #личный кабинет
-      </h2>
+      <b-row align-v="center" class="mb-4 d-none d-md-flex">
+        <b-col md="6" lg="4" xl="3">
+          <h2 class="font-size-4 mb-0">
+            #личный кабинет
+          </h2>
+        </b-col>
+        <b-col md="6" lg="8" xl="9" class="d-flex">
+          <b-dropdown
+            :class="{ desc: sortDesc }"
+            variant="link"
+            class="dropdown-catalog-sort ml-auto"
+            no-caret
+            right
+            block
+          >
+            <template v-slot:button-content>
+              <span class="caption">
+                {{ activeSort.text }}
+              </span>
+              <svg-icon name="caret" width="12" height="12" />
+            </template>
+            <b-dropdown-item
+              v-for="option in sortOptions"
+              :key="option.value"
+              @click="setSort(option)"
+            >
+              {{ option.text }}
+            </b-dropdown-item>
+          </b-dropdown>
+        </b-col>
+      </b-row>
     </div>
     <div class="container">
       <b-row>
@@ -16,29 +44,24 @@
           <account-menu></account-menu>
         </b-col>
         <b-col md="6" lg="8" xl="9">
-          <b-button
-            to="/account"
-            variant="link"
-            class="account-backlink d-md-none"
-          >
-            <svg-icon name="arrow-backlink" width="19" height="7" />
-            <span class="caption">
-              Любимые товары
-            </span>
-          </b-button>
-
-          <b-row>
-            <b-col cols="12" lg="8" xl="7">
-              <p class="mb-lg-4">
-                Оцените товар или&nbsp;оставьте отзыв и&nbsp;заработайте скидку
-                или&nbsp;баллы на&nbsp;следующие покупки
-              </p>
+          <b-row align-v="center" class="mb-2 pb-1 d-md-none">
+            <b-col cols="6">
+              <b-button
+                to="/account"
+                variant="link"
+                class="account-backlink mb-0"
+              >
+                <svg-icon name="arrow-backlink" width="19" height="7" />
+                <span class="caption">
+                  Избранное
+                </span>
+              </b-button>
             </b-col>
-            <b-col cols="12" lg="3" offset-lg="1" offset-xl="2" class="d-flex">
+            <b-col cols="6" class="d-flex">
               <b-dropdown
                 :class="{ desc: sortDesc }"
                 variant="link"
-                class="dropdown-catalog-sort ml-auto mb-3"
+                class="dropdown-catalog-sort ml-auto"
                 no-caret
                 right
                 block
@@ -60,7 +83,7 @@
             </b-col>
           </b-row>
 
-          <b-row class="mb-lg-4">
+          <b-row class="catalog-items mb-lg-4">
             <b-col
               v-for="i in 6"
               :key="`liked-product-${i}`"
@@ -68,7 +91,7 @@
               lg="4"
               class="mb-3 mb-lg-30"
             >
-              <card-liked-product :item="item"></card-liked-product>
+              <card-product :item="item" has-remove></card-product>
             </b-col>
           </b-row>
         </b-col>
@@ -79,13 +102,13 @@
 
 <script>
 import AccountMenu from '@/components/account/AccountMenu'
-import CardLikedProduct from '@/components/cards/CardLikedProduct'
+import CardProduct from '@/components/cards/CardProduct'
 
 export default {
   layout: 'account',
   components: {
     AccountMenu,
-    CardLikedProduct
+    CardProduct
   },
   data() {
     return {
